33af814d9a7080cadaa85e9e94075463ce8a9ecb
FathimaZakiya <zakiyashaik3037@gmail.com>
2026-08-25 20:33:41 +0530

    Implemented booWho() to check whether a value is a Boolean using typeof.

---
+++ practice.js
@@ -0,0 +1,14 @@
+ 
+function booWho(bool) {
+  return typeof bool === "boolean";
+}
+console.log(booWho(true));
+console.log(booWho(false));
+console.log(booWho([1, 2, 3]));
+console.log(booWho([].slice));
+console.log(booWho({ "a": 1 }));
+console.log(booWho(1));
+console.log(booWho(NaN));
+console.log(booWho("a"));
+console.log(booWho("true"));
+console.log(booWho("false"));
